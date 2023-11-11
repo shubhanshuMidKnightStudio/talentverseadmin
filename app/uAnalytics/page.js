6e741a41fbf87b5addce35a8ed36563bdad72283
shubhanshuMidknightstudio <shubhanshu_bravo@midknightstudio>
2023-11-11 23:01:41 +0530

    Gender Chart Update

--- app/uAnalytics/page.js
+++ app/uAnalytics/page.js
@@ -27,49 +27,16 @@ const page = () => {
 
   return (
     <div className=" bg-slate-50 h-auto pb-10">
-      <h1 className="px-8 py-8 text-black text-2xl font-semibold">
+      <h1 className="px-8 py-3 text-black text-2xl font-semibold">
         User Analytics
       </h1>
 
-      <div className="px-10 py-2 flex flex-row">
+      <div className="px-24 py-2 flex flex-row">
         <div className="flex flex-col border-blue border-2 rounded-md bg-white pl-5 w-1/3 pr-5">
-          <h1 className="text-black font-medium px-28 py-4 flex flex-row">
+          <h1 className="text-black font-medium mt-5 px-24 py-1 flex flex-row">
             <MdOutlineSystemUpdateAlt className="text-black mt-1 mr-3" />
             Operating System
           </h1>
-          <table className="w-full ">
-            <thead>
-              <tr className="bg-slate-100">
-                <th className="py-1 text-black text-sm font-normal">Type</th>
-                <th className="py-1 text-black text-sm font-normal">
-                  Total Users
-                </th>
-                <th className="py-1 text-black text-sm font-normal">
-                  Bounce Rate
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="py-1">
-                <td className="text-black text-left pl-5 flex flex-row">
-                  <DiAndroid className="text-orange-600 mr-3 text-xl" />
-                  Android
-                </td>
-                <td className=" text-black text-left pl-5">45,4000</td>
-                <td className=" text-green-600 text-left pl-8">45 %</td>
-              </tr>
-            </tbody>
-            <tbody>
-              <tr className="py-2">
-                <td className="text-black text-left pl-5 flex flex-row">
-                  <DiAppstore className="text-blue-400 mr-3 text-2xl" />
-                  IOS
-                </td>
-                <td className="text-black text-left pl-5">45,4000</td>
-                <td className="text-green-600 text-left pl-8">45 %</td>
-              </tr>
-            </tbody>
-          </table>
           <PieChart width={400} height={200}>
             <Pie
               dataKey="value"
@@ -105,6 +72,7 @@ const page = () => {
                   </text>
                 );
               }}
+              labelLine={false}
             >
               {OSData.map((entry, index) => (
                 <Cell
@@ -115,13 +83,103 @@ const page = () => {
             </Pie>
             <Tooltip />
           </PieChart>
+          <table className="w-full ">
+            <thead>
+              <tr className="bg-slate-100">
+                <th className="py-1 text-black text-sm font-normal">Type</th>
+                <th className="py-1 text-black text-sm font-normal">
+                  Total Users
+                </th>
+                <th className="py-1 text-black text-sm font-normal">
+                  Bounce Rate
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="py-1">
+                <td className="text-black text-left pl-5 flex flex-row">
+                  <DiAndroid className="text-orange-600 mr-3 text-xl" />
+                  Android
+                </td>
+                <td className=" text-black text-left pl-5">45,4000</td>
+                <td className=" text-green-600 text-left pl-8">45 %</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr className="py-2">
+                <td className="text-black text-left pl-5 flex flex-row">
+                  <DiAppstore className="text-blue-400 mr-3 text-2xl" />
+                  IOS
+                </td>
+                <td className="text-black text-left pl-5">45,4000</td>
+                <td className="text-green-600 text-left pl-8">45 %</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
         <div className="flex flex-col border-blue border-2 rounded-md bg-white pl-5 w-1/3 pr-5 ml-5">
-          <h1 className="text-black font-medium px-28 py-4 flex flex-row">
+          <h1 className="text-black font-medium mt-5 px-28 py-1 pt-2 flex flex-row">
             <MdPeople className="text-black mt-1 mr-3" />
             Gender
           </h1>
+          <PieChart width={400} height={200}>
+            <Pie
+              dataKey="value"
+              isAnimationActive={false}
+              data={GenderData}
+              cx={200}
+              cy={100}
+              outerRadius={80}
+              fill="#8884d8"
+              label={({
+                cx,
+                cy,
+                midAngle,
+                innerRadius,
+                outerRadius,
+                percent,
+                index,
+              }) => {
+                console.log("Label Data:", {
+                  cx,
+                  cy,
+                  midAngle,
+                  innerRadius,
+                  outerRadius,
+                  percent,
+                  index,
+                });
 
+                const RADIAN = Math.PI / 180;
+                const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+                const x = cx + radius * Math.cos(-midAngle * RADIAN);
+                const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+                console.log("Label Position:", { x, y });
+
+                return (
+                  <text
+                    x={x}
+                    y={y}
+                    fill="black"
+                    textAnchor={x > cx ? "start" : "end"}
+                    dominantBaseline="central"
+                  >
+                    {`${(percent * 100).toFixed(0)}%`}
+                  </text>
+                );
+              }}
+              labelLine={false}
+            >
+              {OSData.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLOR[index % COLOR.length]}
+                />
+              ))}
+            </Pie>
+            <Tooltip />
+          </PieChart>
           <table className="w-full ">
             <thead>
               <tr className="bg-slate-100">
@@ -161,51 +219,6 @@ const page = () => {
               </tr>
             </tbody>
           </table>
-          <PieChart width={400} height={200}>
-            <Pie
-              dataKey="value"
-              isAnimationActive={false}
-              data={GenderData}
-              cx={200}
-              cy={100}
-              outerRadius={80}
-              fill="#8884d8"
-              label={({
-                cx,
-                cy,
-                midAngle,
-                innerRadius,
-                outerRadius,
-                percent,
-                index,
-              }) => {
-                const RADIAN = Math.PI / 180;
-                const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-                const x = cx + radius * Math.cos(-midAngle * RADIAN);
-                const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-                return (
-                  <text
-                    x={x}
-                    y={y}
-                    fill="black"
-                    textAnchor={x > cx ? "start" : "end"}
-                    dominantBaseline="central"
-                  >
-                    {`${(percent * 100).toFixed(0)}%`}
-                  </text>
-                );
-              }}
-            >
-              {OSData.map((entry, index) => (
-                <Cell
-                  key={`cell-${index}`}
-                  fill={COLOR[index % COLOR.length]}
-                />
-              ))}
-            </Pie>
-            <Tooltip />
-          </PieChart>
         </div>
       </div>
 
